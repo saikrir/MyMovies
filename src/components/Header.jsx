1a1038d7e72d1ffa4555298e97b1a8c5d69d0f8f
Sai Katterishetty <rao.saikrishna@gmail.com>
2018-04-23 22:06:34 -0500

    Fixed Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Header = (props) => {
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">My Movies</h1>
-			<p className="lead">Simple Application to track and follow your favorite movies</p>
+		<div className="col-md-12">
+			<div className="jumbotron">
+				<h1 className="display-10">My Movies</h1>
+				<p className="lead">Simple Application to track and follow your favorite movies</p>
+			</div>
 		</div>
-	); 
+	);
 };
 export default Header;
